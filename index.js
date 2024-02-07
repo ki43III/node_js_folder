c3786d85a4f6d95776c1db3f49dc34fa75e43bbd
ki43 <k22103kk@aitech.ac.jp>
2024-02-07 23:28:03 +0900

    match＿修正

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
+let waitingMatch = null;
+
 // POSTリクエストを処理するエンドポイント
 app.post('/post', (req, res) => {
     console.log('POSTリクエストを受信しました:', req.body);
@@ -15,9 +17,16 @@ app.post('/post', (req, res) => {
 // GETリクエストを処理するエンドポイント
 app.get('/get', (req, res) => {
     const input_match = req.query.input_match;
-    if (input_match === '123') {
+    if (waitingMatch === null) {
+        // 待機中のマッチが存在しない場合
+        waitingMatch = input_match;
+        res.send('Waiting for match...');
+    } else if (waitingMatch === input_match) {
+        // 待機中のマッチと入力が一致する場合、マッチ成功
+        waitingMatch = null; // マッチ成功したので待機中のマッチを破棄
         res.send('Match successful');
     } else {
+        // 他のマッチと入力が一致しない場合、マッチ失敗
         res.send('Match failed');
     }
 });
