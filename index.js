fe82f69191df7ebea70e0a3ca1e50434644436e2
ki43 <k22103kk@aitech.ac.jp>
2024-02-09 02:05:03 +0900

    原始的な方法で中継を実現

--- index.js
+++ index.js
@@ -5,10 +5,33 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+let resistdata1s = [];
+let requestdat2s = [];
+
 
 app.post('/', (req, res) => {
-    console.log('JSONデータを受信しました:', req.body);
-    res.send('JSONデータを受信しました');
+    //jsonデータの中身を表示
+    //console.log(req.body.value);
+    if (req.body.key == 1) {
+        resistdata1s = req.body;
+    }
+
+    if (req.body.key == 2) {
+        requestdat2s = req.body;
+    }
+
+    if (req.body.key == 0) {
+        if (req.body.name == 0) {
+            res.json(resistdata1s);
+        }
+        if (req.body.name == 1) {
+            res.json(requestdat2s);
+        }
+    }
+
+    //console.log('JSONデータを受信しました:', req.body);
+    //console.log(res.body.length);
+
 });
 
 app.listen(port, () => {
