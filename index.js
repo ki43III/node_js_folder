cb8ee4f064440c9470cd3744fdb11842e6e55256
ki43 <k22103kk@aitech.ac.jp>
2024-02-08 22:56:46 +0900

    やはりudpの方が良いかも

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-const WebSocket = require('ws');
+const dgram = require('dgram');
+const server = dgram.createSocket('udp4');
 
-const wss = new WebSocket.Server({ port: 8080 });
+const PORT = 12345;
 
-let clients = [];
-
-wss.on('connection', function connection(ws) {
-    clients.push(ws);
-
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
-        // メッセージを他のクライアントに送信
-        clients.forEach(client => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(message);
-            }
-        });
-    });
-
-    ws.on('close', function close() {
-        clients = clients.filter(client => client !== ws);
-    });
+server.on('message', (msg, rinfo) => {
+    console.log(`Received message from ${rinfo.address}:${rinfo.port}: ${msg}`);
 });
+
+server.on('listening', () => {
+    const address = server.address();
+    console.log(`UDP server listening on ${address.address}:${address.port}`);
+});
+
+server.bind(PORT);
