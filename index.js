0272fa5f075d2b7f1652e62179ba8feaa77f2a22
ki43 <k22103kk@aitech.ac.jp>
2024-02-09 18:58:57 +0900

    ローカル環境での中継を実現

--- index.js
+++ index.js
@@ -5,33 +5,34 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
-let resistdata1s = [];
-let requestdat2s = [];
-
+let resistdata1s = undefined;
+let requestdat2s = undefined;
 
 app.post('/', (req, res) => {
     //jsonデータの中身を表示
-    //console.log(req.body.value);
+
     if (req.body.key == 1) {
         resistdata1s = req.body;
+        res.json(requestdat2s);
     }
 
     if (req.body.key == 2) {
         requestdat2s = req.body;
+        res.json(resistdata1s);
     }
-
-    if (req.body.key == 0) {
-        if (req.body.name == 0) {
-            res.json(resistdata1s);
+    /*
+        if (req.body.key == 0) {
+            if (req.body.name == 0) {
+                res.json(resistdata1s);
+            }
+            if (req.body.name == 1) {
+                res.json(requestdat2s);
+            }
         }
-        if (req.body.name == 1) {
-            res.json(requestdat2s);
-        }
-    }
-
-    //console.log('JSONデータを受信しました:', req.body);
-    //console.log(res.body.length);
-
+    */
+    /*console.log(req.body);
+    res.json(req.body);
+    */
 });
 
 app.listen(port, () => {
